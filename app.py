80ac770c247f80cebed6c10910c79034191d36a3
bkarthi20 <bkarthi201999@gmail.com>
2026-06-27 16:29:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+print("Welcome to Jenkins Pipeline")
+print("Welcome to Jenkins Pipeline")
+
+name = "Renganayaki"
+print("Hello", name)
+
+a = 100
+b = 200
+print("Total =", a + b)
+ name = "Renganayaki" print("Hello", name) a = 100 b = 200 print("Total =", a+b)
